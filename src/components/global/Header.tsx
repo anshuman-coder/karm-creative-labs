74055a9c468a50f99396dbc177065060e5af0dd3
anshuman-coder <anshurockingboy23@gmail.com>
2025-10-26 01:11:09 +0530

    header component changes

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -152,7 +152,7 @@ const DesktopHeader: FC<DesktopHeaderProps> = ({ handleNavClick }) => {
       className="flex w-full items-center justify-start"
     >
       <Logo className="w-16 h-8 xl:w-40 xl:h-16 2xl:w-56 2xl:h-24" />
-      <div className="bg-brand-text font-rebond absolute top-0 left-1/2 sm:-translate-x-2/5 md:-translate-x-1/2 max-w-3xl -translate-x-1/2 -translate-y-1/5 rounded-full text-lg font-semibold text-white">
+      <div className="bg-brand-text font-rebond absolute top-1/2 left-1/2 sm:-translate-x-2/5 md:-translate-x-1/2 max-w-3xl -translate-x-1/2 -translate-y-1/2 rounded-full text-lg font-semibold text-white">
         <nav className="md: laptop:gap-x-10 flex w-full items-center justify-between gap-x-6 rounded-full px-7 py-4 shadow-2xl lg:gap-x-4">
           {NavMenuItems.map((item) => (
             <a
