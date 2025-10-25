36bcab7d770ae922c8b93f238249b8721fc2bdba
anshuman-coder <anshurockingboy23@gmail.com>
2025-10-26 04:01:01 +0530

    changing the description content

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -10,18 +10,14 @@ const Description: FC<DescriptionProps> = ({ className = "" }) => {
   return (
     <div
       className={clsx(
-        "flex flex-col justify-start items-start flex-1 w-full max-w-[1330px] rounded-3xl overflow-hidden",
+        "flex flex-col justify-start items-start flex-1 w-full overflow-hidden",
         className,
       )}
     >
       <div className="flex-1 relative flex flex-col mx-auto justify-between bg-bg-banner rounded-3xl md:rounded-none w-full">
         <div className="flex-1 pl-3 sm:px-12 pr-5 pt-7 sm:pt-14 md:pt-10 pb-28 md:pb-0 laptop:pb-80 2xl:pb-72">
           <p className="font-rebond text-2xl md:text-4xl laptop:text-6xl leading-[1.2] font-semibold text-black">
-            We are a creative-first agency helping brands design, build, and
-            scale with purpose. Our team blends strategy, design, and technology
-            to craft experiences that inspire and drive growth. Whether you need
-            a single expert or a full team, we deliver flexible solutions
-            tailored to your goals.
+            We are a creative-first lab helping brands design, build, and scale with purpose. Our team blends strategy, design, analytics, and technology to craft meaningful experiences that inspire and drive growth. We turn insights into action, translating data into creative opportunities that deliver measurable impact. Every project is rooted in curiosity, collaboration, and innovation. Whether you need a single expert or a full team, we provide flexible solutions tailored to your goals.
           </p>
         </div>
         <div className="w-full overflow-hidden absolute bottom-0 left-0 right-0 pb-4 md:pb-6">
